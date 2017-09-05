d31445e2804a17a6dff5d70d61dc0b5ee710b73f
RajeshVerma <rjsverma.rv@gmail.com>
2017-09-05 16:44:31 +0530

    first commit

---
+++ allFunc.js
@@ -0,0 +1,44 @@
+(function (btn) {
+    function intializeAll(obj) {
+        this.hostEl = obj.hostEl;
+        this.targetEl = obj.targetEl || '';
+        this.eventListener = obj.eventListener;
+        this.appendEl = obj.appendEl || '';
+        this.removeEl = obj.removeEl || '';
+        this.createEl = obj.createEl || '';
+        this.eventListener();
+    }
+
+    function CreateInstance() { };
+    CreateInstance.prototype.initali = intializeAll;
+    CreateInstance.prototype.create = function (obj) {
+        switch (obj.module) {
+            case "intializeAll": this.initali;
+                break;
+            case "module2": this.module2;
+                break;
+        }
+        return this.initali(obj);
+    }
+
+    var newObject = new CreateInstance();
+    newObject.create({
+        module: 'intializeAll',
+        hostEl: btn,
+        eventListener: function () {
+            var self = this;
+            this.hostEl.addEventListener('click', function () {
+                console.log("hello");
+                self.createEl();
+            });
+        },
+        createEl: function () {
+            var btn = document.createElement('button');
+            btn.innerText = 'NEW BUTTON';
+            document.body.appendChild(btn);
+        },
+        removeEl: function () {
+
+        }
+    });
+})(document.getElementById('Btn'));
